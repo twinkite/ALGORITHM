fd484aa8dfc2502f1d56952bd2e7809e6c3bbb34
twinkite <yeonni_9657@naver.com>
2022-03-12 16:08:05 +0900

    11652.cpp

--- barkingdog/0x0F/11652.cpp
+++ barkingdog/0x0F/11652.cpp
@@ -8,10 +8,10 @@ int main(){
     ios_base::sync_with_stdio(false);
 
     int N;
-    map <string, int> m;
+    map <long long, int> m;
     cin>>N;
     while(N--){
-        string s;
+        long long s;
         cin>>s;
         if(m.find(s)!=m.end()){
             m[s]+=1;
@@ -20,15 +20,13 @@ int main(){
         }
     }
     int ans=0;
-    string temp;
+    long long temp;
     for(auto iter = m.begin(); iter!=m.end();iter++){
         if(iter->second >ans){
             ans=iter->second;
             temp=iter->first;
         } else if(iter->second == ans){
-            long long temp1 = stoll(temp);
-            long long temp2 = stoll(iter->first);
-            if(temp1>temp2){
+            if(temp>iter->first){
                 ans=iter->second;
                 temp=iter->first;
             }
