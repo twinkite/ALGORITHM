a1e2d7ab9fea47a8cd9987b159089c79f6e21540
twinkite <yeonni_9657@naver.com>
2022-05-09 21:00:55 +0900

    220509

---
+++ BOJ/17298.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+#define pii pair<int, int>
+using namespace std;
+
+stack<pii> s;
+int ans[1111111];
+int N;
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin>>N;
+    int temp;
+    for(int i=1;i<=N;i++){
+        cin>>temp;
+        while(!s.empty()&&s.top().first<=temp){
+            ans[s.top().second]=temp;
+            s.pop();
+        }
+        s.push({temp,i});
+    }
+    while(!s.empty()){
+        ans[s.top().second]=-1;
+        s.pop();
+    }
+    for(int i=1;i<=N;i++){
+        cout<<ans[i]<<' ';
+    }
+
+    return 0;
+}
